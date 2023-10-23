cab83c7f647ee463e26e0f52e7f0568d7b5fb944
caijda <145417979+caijda@users.noreply.github.com>
2023-10-22 20:53:43 -0600

    Update to UserService and UserLoginApplication

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -1,36 +1,12 @@
 package com.coderscampus;
 
-import java.util.Scanner;
-
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
 
 		UserService userService = new UserService();
 		User[] users = userService.createUserArray();
-		String[] userInput = new String[2];
-		int attempts = 0;
-		int userIndex;
-		Scanner scanner = new Scanner(System.in);
-
-		while (attempts < 5) {
-			System.out.println("Enter your email:");
-			userInput[0] = scanner.nextLine();
-			System.out.println("Enter your password:");
-			userInput[1] = scanner.nextLine();
-			userIndex = userService.validatingInput(userInput, users);
-			if (userIndex != 4) {
-				System.out.println("Welcome: " + users[userIndex].getName());
-				break;
-			} else if (attempts < 4) {
-				System.out.println("Invalid login, please try again");
-				attempts++;
-			} else {
-				System.out.println("Too many failed login attempts, you are now locked out.");
-				attempts++;
-			}
-		}
-		scanner.close();
+		userService.userInput(users);
 	}
 
 }
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class UserService {
 
@@ -51,5 +52,31 @@ public class UserService {
 			}
 		}		
 		return i;
+	}
+	
+	public void userInput (User[] users) {
+		String[] userInput = new String[2];
+		int attempts = 0;
+		int userIndex;
+		Scanner scanner = new Scanner(System.in);
+
+		while (attempts < 5) {
+			System.out.println("Enter your email:");
+			userInput[0] = scanner.nextLine();
+			System.out.println("Enter your password:");
+			userInput[1] = scanner.nextLine();
+			userIndex = validatingInput(userInput, users);
+			if (userIndex != 4) {
+				System.out.println("Welcome: " + users[userIndex].getName());
+				break;
+			} else if (attempts < 4) {
+				System.out.println("Invalid login, please try again");
+				attempts++;
+			} else {
+				System.out.println("Too many failed login attempts, you are now locked out.");
+				attempts++;
+			}
+		}
+		scanner.close();
 	}
 }
